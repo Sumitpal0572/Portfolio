d2398e55fdca54dd41ebe9749ecccd7fb2679d06
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-06 23:42:33 +0530

    Components fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import react from "react";
+import React from "react";
 import "./App.css";
-
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Skill from "./components/Skills/Skill";
+import Education from "./components/Education/Education";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Work from "./components/Work/Work";
 function App() {
   return (
-    <>
-      <h1 className="text-red-500">Hello world by Sumit</h1>
-    </>
+    <div className="bg-[#050414]">
+      <div className=""></div>
+      <div className="relative pt-20">
+        <Navbar />
+        <About />
+        <Skill />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
+    </div>
   );
 }
 
