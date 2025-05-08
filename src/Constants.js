d4c70b598c3fd42033d47c3ba13e1b7952867b3a
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-08 19:28:11 +0530

    Constant page added

---
+++ src/Constants.js
@@ -0,0 +1,17 @@
+export const SkillsInfo = [
+    {
+        title: 'Frontend',
+        skills: [
+            { name: 'HTML' },
+            { name: 'CSS' },
+            { name: 'Javascript' },
+            { name: 'REACT JS' },
+            { name: 'SASS' },
+            { name: 'Angular' },
+            { name: 'Next Js' },
+            { name: 'Tailwind Css' },
+            { name: 'Materail UI' },
+            { name: 'Chakra UI' },
+        ],
+    },
+]
