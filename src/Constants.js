09d34995ed11a0dc213a690290a26c727373a9af
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-09 23:10:53 +0530

    Tools and Languages added

--- src/Constants.js
+++ src/Constants.js
@@ -9,8 +9,10 @@ export const SkillsInfo = [
             { name: 'SASS' },
             { name: 'Angular' },
             { name: 'Next Js' },
+            { name: 'Redux' },
             { name: 'Tailwind Css' },
             { name: 'Materail UI' },
+            { name: 'BootStrap' },
             { name: 'Chakra UI' },
         ],
     },
@@ -25,6 +27,28 @@ export const SkillsInfo = [
             { name: 'Mongo DB' },
             { name: 'Firebase' },
             { name: 'MySQL' },
+        ],
+    },
+
+    {
+        title: 'Languages',
+        skills: [
+            { name: 'Javascript' },
+            { name: 'Python' },
+            { name: 'Typescript' },
+        ],
+    },
+
+    {
+        title: 'Tools',
+        skills: [
+            { name: 'Git' },
+            { name: 'Github' },
+            { name: 'VS Code' },
+            { name: 'Postman' },
+            { name: 'Compass' },
+            { name: 'Vercel' },
+            { name: 'Netlify' },
 
         ],
     },
