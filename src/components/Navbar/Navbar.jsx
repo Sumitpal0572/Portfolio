b0bdd83b8c40959081607a5c12d2488260f50f33
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-10 23:37:09 +0530

    icons added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-transparent">
+      <div className="text-white py-5 flex justify-between itmes-center">
+        <div>
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Sumit</span>
+          <span className="text-[#8245ec]">&lt;</span>
+          <span className="text-white">Pal</span>
+          <span className="text-[#8245ec]">&gt;</span>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
