38f648090bedea8b2c8a619469486c7117f0dfaf
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-11 21:18:57 +0530

    navbar created

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Navbar = () => {
+  const [isOpen, setisOpen] = useState(false);
+  const [activeSection, setactiveSection] = useState("");
+
+  const handleMenuItemsClick = (sectionId) => {
+    setactiveSection(sectionId);
+  };
+
   const menuItems = [
     { id: "about", label: "About" },
     { id: "skills", label: "Skills" },
@@ -13,7 +20,7 @@ const Navbar = () => {
 
   return (
     <nav className="bg-transparent">
-      <div className="text-white py-5 flex justify-between itmes-center">
+      <div className="text-white py-5 flex justify-between items-center">
         {/*   logo */}
         <div className="text-lg font-semibold cursor-pointer">
           <span className="text-[#8245ec]">&lt;</span>
@@ -24,10 +31,18 @@ const Navbar = () => {
         </div>
 
         {/* Daesktop menu  */}
-        <ul className="md:flex space-x-8 text-grey-300">
+        <ul className="md:flex space-x-8 text-gray-300">
           {menuItems.map((item) => (
-            <li key={item.id} className="">
-              <button>{item.label}</button>
+            <li
+              key={item.id}
+              className={`cursor:default hover:text-[#8245ec] ${
+                activeSection === item.id ? "text-[#8245ec]" : ""
+              }
+            `}
+            >
+              <button onClick={() => handleMenuItemsClick(item.id)}>
+                {item.label}
+              </button>
             </li>
           ))}
         </ul>
