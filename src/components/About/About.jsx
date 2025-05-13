690ec659ec3c2cbd584fd5e1fdaf94a17bb2fa2f
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-13 13:34:59 +0530

    hero section updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <section
+      id="about"
+      className="py-4 px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between item-center">
+        {/* left side */}
 
-export default About
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* greeting  */}
+
+          <h1 className="text-3xl sm:text-5xl md:text-6xlfont-bold leading-tight text-white mb-2">
+            Hi, I am
+          </h1>
+          {/* name  */}
+          <h2 className="tetx-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight">
+            Sumit Pal
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
