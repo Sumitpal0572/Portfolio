543232bcdda7854eeae77afe9a24dc246c8ab05f
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-13 12:38:06 +0530

    animation added

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  theme: {
+    extend: {
+      keyframes: {
+        blob: {
+          '0': { transform: 'scale(1)' },
+          '33': { transform: 'scale(1.2)' },
+          '66': { transform: 'scale(0.8)' },
+          '100': { transform: 'scale(1)' },
+
+        },
+      },
+      animation: {
+        blob: 'blob 10s infinite',
+      }
+    }
+  },
+
   plugins: [react(), tailwindcss()],
 })
