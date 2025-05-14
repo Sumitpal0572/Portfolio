a7caa8c1a1230b158043ab2a37a27dc9a8305323
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-14 13:02:58 +0530

    Profile image added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
+import Tilt from "react-parallax-tilt";
 
 const About = () => {
   return (
@@ -59,6 +60,18 @@ const About = () => {
           >
             Download CV
           </a>
+        </div>
+
+        {/* right side  */}
+
+        <div className="md:w-1/2 flex justify-center  md:justify-end ">
+          <Tilt className="w-48 h-48 sm:w-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full ">
+            <img
+              src="https://avatars.githubusercontent.com/u/148610135?v=4"
+              alt="Profile image"
+              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+            />
+          </Tilt>
         </div>
       </div>
     </section>
