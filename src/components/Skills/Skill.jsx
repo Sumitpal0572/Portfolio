5c8a41c4aafe1f3d9c87c5d0917cda0e355b54f3
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-15 13:22:24 +0530

    Skills section added

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { SkillsInfo } from "../../Constants";
 
 const Skill = () => {
   return (
-    <div>Skill</div>
-  )
-}
+    <section
+      id="skills"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient "
+    >
+      <div className="text-center mb-8">
+        <h2 className="text-3xl sm:text-4xl font-bold text-white ">SKILLS</h2>
+        <div className="w-24 h-1 bg-[#8245ec] mx-automt-2"></div>
+        <p className="text-gray-400 text-lg mt-4 font-semibold">
+          A Collection of Technical Skills and expertise honed through various
+          projects ad experinces
+        </p>
+      </div>
 
-export default Skill
+      {/* //skills categories */}
+
+    </section>
+  );
+};
+
+export default Skill;
