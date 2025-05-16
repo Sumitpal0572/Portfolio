24716e91402649a29f66982b264b592a3b0d23bd
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-16 12:38:27 +0530

    Tilt effect added

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -6,7 +6,7 @@ const Skill = () => {
   return (
     <section
       id="skills"
-      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient "
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient .clip-path-custom "
     >
       <div className="text-center mb-8">
         <h2 className="text-3xl sm:text-4xl font-bold text-white ">SKILLS</h2>
