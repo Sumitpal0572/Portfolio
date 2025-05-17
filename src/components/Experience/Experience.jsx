28ccac117ff12a4845e34a0eb0ccc55fae8502d3
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-17 11:46:20 +0530

    Experience section added

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -11,11 +11,14 @@ const Experience = () => {
       <div className="text-center mb-16 ">
         <h2 className="text-4xl font-bold text-white"> EXPERIENCES</h2>
         <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
-        <p className="">
+        <p className="mt-5 text-gray-400 text-lg font-semibold">
           The Collection of my Work Experiences and the roleS I have taken in
           various Organizations.
         </p>
       </div>
+{/* experiences  */}
+
+
     </section>
   );
 };
