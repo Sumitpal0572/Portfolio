6dc5a6fb92065f335873b92597f268151f03de00
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-18 23:04:59 +0530

    Project section updated

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -7,7 +7,14 @@ const Work = () => {
       className="py-24 pb-24 px-[#12vw] md:px-[7vw] lg:px-[20vw] font-sans relative"
     >
       {/* section title  */}
-      
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-bold text-white">PROJECTS</h2>
+        <div className="w-32 h-1 bg-purple-500 mx-auto mt-4"></div>
+        <p className="text-gray-400 mt-4 text-lg font-semibold">
+          A Showcase of the Projects, I have Worked on, Highlighting my skill
+          and Experiences in various Technology.
+        </p>
+      </div>
     </section>
   );
 };
