e9d12648805f77fc9099fe4717a16a1f45bb0f6e
Sumitpal0572 <asmyselfsumit@gmail.com>
2025-05-21 11:45:40 +0530

    project section done

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -67,9 +67,9 @@ const Work = () => {
       {/* Model container  */}
 
       {selectedProject && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90 p-4">
-          <div className="bg-gray-900 rounded-xl shadow-2xl lg:w-full w-[90%] max-w-3xl overflow-hidden relative">
-            <div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 p-4">
+          <div className="bg-gray-900 rounded-xl shadow-2xl w-[90%] max-w-2xl max-h-[85vh] overflow-y-auto relative">
+            <div className="flex justify-end p-4">
               <button
                 onClick={handleCloseModel}
                 className="text-white text-3xl font-bold hover:text-purple-500"
@@ -77,13 +77,49 @@ const Work = () => {
                 &times;
               </button>
             </div>
-            <div className="flex flex-col">
-              <div className="w-full flex justify-center bg-gray-900 px-4">
+            <div className="flex flex-col items-center">
+              <div className="w-full flex  bg-gray-900 px-4">
                 <img
                   src={selectedProject.image}
                   alt={selectedProject.title}
-                  className="lg:w-full w-[95%] object-conatin rounded-xl shadow-2xl"
+                  className="lg:w-full w-[95%] object-contain rounded-xl shadow-2xl"
                 />
+              </div>
+              <div className="lg:p-8 p-6">
+                <h3 className="lg:text-3xl font-bold text-white mb-4 text-md">
+                  {selectedProject.title}
+                </h3>
+                <p className="text-gray-400 mb-6 lg:text-base text-xs">
+                  {selectedProject.description}
+                </p>
+                <div className="flex flex-wrap gap-2 mb-6">
+                  {selectedProject.tags.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="bg-[#251f38] text-xs font-semibold text-purple-500 rounded-full px-2 py-1 "
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <div className="flex gap-4">
+                  <a
+                    href={selectedProject.github}
+                    target="_blank"
+                    rel="nooperner noreffer"
+                    className="w-1/2 bg-gray-400 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Code
+                  </a>
+                  <a
+                    href={selectedProject.webapp}
+                    target="_blank"
+                    rel="nooperner noreffer"
+                    className="w-1/2 bg-purple-600 hover:bg-purple-800 text-gray-400 lg:px-6 lg:py-2 px-2 py-1 rounded-xl lg:text-xl text-sm font-semibold text-center"
+                  >
+                    View Live
+                  </a>
+                </div>
               </div>
             </div>
           </div>
